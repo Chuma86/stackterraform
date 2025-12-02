b898892e336f6637bd0eb386dcf15898fdb7a7f6
Chuma86 <enekebechuma@gmail.com>
2025-12-02 14:09:09 -0800

    Update stackterraform project

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 ##Install the needed packages and enable the services(MariaDb, Apache)
 sudo dnf upgrade -y
 sudo dnf install -y httpd wget php-fpm php-mysqli php-json php php-devel
-sudo dnf install mariadb105-server -y
+sudo dnf install mariadb105-server git -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl is-enabled httpd

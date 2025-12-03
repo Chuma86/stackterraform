79d5b9c65613c427bf87e7380484c638f4c36933
Chuma86 <enekebechuma@gmail.com>
2025-12-03 04:13:07 -0800

    Update inspector.tf after plan and troubleshooting

--- instances/inspector.tf
+++ instances/inspector.tf
@@ -1,8 +1,8 @@
 
 # AWS Inspector2 Enabler
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 
-resource "aws_inspector2_enabler" "default" {
-  account_ids    = [data.aws_caller_identity.current.account_id]
-  resource_types = ["EC2", "ECR"]
-}
+#resource "aws_inspector2_enabler" "default" {
+#  account_ids    = [data.aws_caller_identity.current.account_id]
+#  resource_types = ["EC2", "ECR"]
+#}
